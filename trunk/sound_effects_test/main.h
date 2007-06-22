5bad7508bd9331cb022bfa962da62fcebf12b0eb
smnsmn1979 <smnsmn1979@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-06-22 16:21:03 +0000

    Removed file/folder

--- trunk/sound_effects_test/main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <stdio.h>
-
-typedef  void eff_parameters;
-//Объявление функции из динамической библиотеки wav. Сама библиотека будет загружена при старте приложения.
-extern "C" __declspec(dllimport) void sound_effect(eff_parameters* parameters);

b859f7e80fdd3a79aa617967280c74dc2fd72647
pigulik <pigulik@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-03 10:18:36 +0000

    today added structure for data file, added in structure component sample[]. now you can read sample of mono wave file pcm riff (8 bite).

--- Sp/wav/wav.cpp
+++ Sp/wav/wav.cpp
@@ -1,7 +1,6 @@
 // This is the main DLL file.
 
 #include "wav.h"
-
 #ifdef _MANAGED
 #pragma managed(push, off)
 #endif
@@ -14,7 +13,43 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 //TODO:
 extern "C" __declspec(dllexport) void read(read_parameters* parameters)
 {
-    printf("Hello world!\n");
+ char* name1="I:\\virtualdj\\Sp\\sound\\asteroidexplode.wav";
+	char *name;
+	int i;
+	i=0;
+	printf("enter name of .wav file\n");
+	printf("Hello world!\n");
+	FILE * f;
+	TitleWave tw;
+	
+	f=fopen(name1,"r");
+	if ( f==0 ) { printf("Cannot open file - %s\n",name1); return; }
+	fread(&tw,sizeof(TitleWave),1,f);
+	fclose(f);
+	printf("LEN RIFF\t - %ld\n", tw.len_riff );
+	if ( strncmp(tw.id_riff,"RIFF",4)!=0 )
+		printf("problem - identificator RIFF\n");
+		printf("LEN Chuck\t - %ld\n", tw.len_chuck );
+	
+	if ( strncmp(tw.id_chuck,"WAVE",4)!=0 )
+		printf("problem - identificator CHUCK\n");
+	if ( strncmp(tw.fmt,"fmt ",4)!=0 )
+		printf("problem - identificatorÍ FMT\n");
+	printf("Type\t\t - %d\n", tw.type );
+	printf("Channels\t - %d\n", tw.channels );
+	printf("Sample Per Sec\t - %d\n", tw.freq );
+	printf("Bytes Per Sec\t - %d\n", tw.bytes );
+	printf("Bits\t\t - %d\n", tw.bits );
+	printf("Aligned\t\t - %d\n", tw.align );
+	printf("LEN Data\t - %ld\n", tw.len_data );
+	if ( strncmp(tw.id_data,"data",4)!=0 )
+		printf("problem - identificator DATA\n");
+	while (tw.sample[i]!=0)
+	{
+		printf("sample\t - %d\n",tw.sample[i]);
+		i=i+1;
+	}
+	//printf("sample\t - %d\n",tw.fg);
 }
 
 //TODO:
--- Sp/wav/wav.h
+++ Sp/wav/wav.h
@@ -7,6 +7,58 @@
 //TODO:
 typedef  void read_parameters;
 typedef  void write_parameters;
+#pragma pack(push,1)
+typedef struct {
+	byte t1;
+	short int t2;
+	} IDType;
+typedef struct {
+	char id[4]; //- идентификатор файла = "RIFF" = 0x46464952
+	long len[4];   //- длина файла без этого заголовка
+	} IDRiff;
+typedef struct {
+	char id[4];  //- идентификатор = "WAVE" = 0x45564157
+	char fmt[4]; //- идентификатор = "fmt " = 0x20746D66
+	long len;    //- длина этого куска WAV - файла,
+	} IDChuckWave;
+typedef struct {
+        int type;   //- тип звуковых данных, бывает - !!!
+					//1 - просто выборка;
+					//0x101 - IBM mu-law;
+                    //0x102 - IBM a-law;
+                    //0x103 - ADPCM.
+        int channels; //- число каналов 1/2 - !!!
+        long SamplesPerSec; //- частота выборки - !!!
+        long AvgBytesPerSec; //- частота выдачи байтов
+        int align; //- выравнивание
+        int bits; //- число бит на выборку  - !!!
+	} IDWave;
+typedef struct {
+	char id[4]; //- идентификатор ="data" =0x61746164
+        long len; //  - длина выборки ( кратно 2 )
+	} IDSampleWave;
+typedef struct {
+	char id_riff[4];//- идентификатор файла = "RIFF" = 0x46464952
+    long len_riff;//- длина файла без этого заголовка
+
+	char id_chuck[4];//- идентификатор = "WAVE" = 0x45564157
+	char fmt[4];//- идентификатор = "fmt " = 0x20746D66
+	long len_chuck; //- длина этого куска WAV - файла,
+
+	short int  type; //- тип звуковых данных
+	short int  channels;//- число каналов
+	long freq;//- частота выборки 
+	long bytes;//- частота выдачи байтов
+	short int  align;//- выравнивание
+	short int  bits;//- число бит на выборку
+
+	char id_data[4];//- идентификатор ="data" =0x61746164
+	long len_data;//  - длина выборки
+	//byte fg;
+	byte sample[350];
+	//short int sample[300];
+	} TitleWave;
+#pragma pack(pop)
 
 extern "C" __declspec(dllexport) void read(read_parameters* parameters);
 

a7631eccc50a9eb2f0385f2f70ceac6e4d8301cf
IvannikovaE <IvannikovaE@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-06 09:31:59 +0000

    сделано пропорциональное изменение графика в зависимости от размеров окна.

--- Sp/test1/Form1.h
+++ Sp/test1/Form1.h
@@ -1,7 +1,9 @@
 
 #pragma once
-const int m = 6;
-const int t = 13;
+
+const int SizeFormH = 6;
+const int SizeFormW = 13;
+
 namespace test1 {
 
 	using namespace System;
@@ -11,61 +13,73 @@ namespace test1 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for Form1
-	///
-	/// WARNING: If you change the name of this class, you will need to change the
-	///          'Resource File Name' property for the managed resource compiler tool
-	///          associated with all .resx files this class depends on.  Otherwise,
-	///          the designers will not be able to interact properly with localized
-	///          resources associated with this form.
-	/// </summary>
-	int const n = 2;
-	int a[10];
+	int const NumberTabs = 2;
+	int const NumberGraphics = 2;
+	int const arraySise = 5;
+	int _array[arraySise];
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
 	public:
-		Form1(int n)
+		Form1(int NumberTabs)
 		{
-			tabPage = gcnew array<System::Windows::Forms::TabPage^>(n);
-			InitializeComponent(n);
-			//
-			//TODO: Add the constructor code here
-			//
+			tabPage = gcnew array<System::Windows::Forms::TabPage^>(NumberTabs);
+			InitializeComponent(NumberTabs);
 		}
 
 		void pagePaint(Object^ /*sender*/, PaintEventArgs^ e)
 		{
 			Graphics^ g = e->Graphics;     
-			//g->DrawLine( System::Drawing::Pens::Black, Point(30,30), Point(40,30));
-			
-			a[0] = 40; a[1] = 36; a[2] = 56; a[3] = 45; a[4] = 80;
-			int k = 0;
+			_array[0] = 40; _array[1] = 36; _array[2] = 56; _array[3] = 45; _array[4] = 80;
+			int const indent = 30;
+			int k = indent;
 			int w,h;
 			Rectangle rect = e->ClipRectangle;
-			w = rect.Width;
-			h = rect.Height;
+			w = (rect.Width);
+			h = (rect.Height);
 			g->FillRectangle(Brushes::White, 0, 0, w, h);
 			//g->DrawLine(Pens::Black, 0, 0, w, h);
-			const int nn = 5;
-			float koefW ;
-			float koefH ;			
-			for (int j = 1; j <= nn; j++)
-			{	
-				koefW = w/nn;
-				if (a[j] != 0)
-				{
-					koefH = h/(a[j]);				
-				}
-				
-				g->FillEllipse(Brushes::Black, Rectangle(koefW*(j+k),koefH*(240-a[j-1]),4,4));
-				if(j == nn) break;
-				//g->DrawLine(Pens::Black, (koefW*(j+k)),(koefH*(240-a[j-1])), (koefW*((j+1)+k+15)), (koefH*(240-a[j])));
-
-				k = k+15;
-			}
 			
-			Invalidate();
+			// to make a calculation coefficient of stretching and pressing
+			float koefW = w/arraySise;
+			float koefH;
+			int max = _array[0];
+			for (int i = 1; i < arraySise; i++)
+			{
+				if (_array[i] > max)
+				{
+					max = _array[i];
+				}
+			}
+			if (max != 0)
+			{
+				koefH = (h-h/20)/max;
+			}
+
+			// draw graphics
+			for (int j = 1; j < arraySise; j++)
+			{	
+				g->FillEllipse(Brushes::Black, Rectangle((j+k),(h-koefH*_array[j-1]),4,4));
+				g->DrawLine(Pens::Black, Point((j+k),(h-koefH*_array[j-1])), Point((j+k+koefW), (h-koefH*_array[j])));
+				k = k+koefW;
+			}
+
+			// draw coordinate line
+			g->DrawLine(Pens::Black, Point(indent, h-indent), Point(indent,10));
+			g->DrawLine(Pens::Black, Point(indent, h-indent), Point(w-10,h-indent));
+
+			// draw direction of coordinate line
+			g->DrawLine(Pens::Black, Point(w-10-5, h-indent-2), Point(w-10,h-indent));
+			g->DrawLine(Pens::Black, Point(w-10-5, h-indent+2), Point(w-10,h-indent));
+			g->DrawLine(Pens::Black, Point(indent-2,10+5), Point(indent,10));
+			g->DrawLine(Pens::Black, Point(indent+2,10+5), Point(indent,10));
+
+			// signature of value
+			for(int i = 1; i <= arraySise; i++)
+			{
+				g->DrawLine(Pens::Black, Point(indent+i*koefW,h-(indent-2)), Point(indent+i*koefW, h-(indent+2)));
+				g->DrawLine(Pens::Black, Point(indent-2, h-_array[i]*koefH), Point(indent+2, h-_array[i]*koefH));
+			}
+			//Invalidate();
 		}
 
 	protected:
@@ -80,6 +94,13 @@ namespace test1 {
 			}
 		}
 
+        virtual void OnSizeChanged (EventArgs^ e) override
+		{
+			Invalidate();
+			Form::OnSizeChanged(e);
+		}
+
+
 	protected: 
 	
 
@@ -87,15 +108,8 @@ namespace test1 {
 	private: PrintDialog^  printDialog1;
 	private: TabControl^  tabControl1;
 	private: array<TabPage^> ^ tabPage;
-
-
-
-
-
-
-
-
-
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
 
 	private:
 		/// <summary>
@@ -108,12 +122,12 @@ namespace test1 {
 		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
 		/// </summary>
-		void InitializeComponent(int n)
+		void InitializeComponent(int NumberTabs)
 		{
 			int i;
 			this->printDialog1 = (gcnew System::Windows::Forms::PrintDialog());
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
-			for (i = 0; i < n; i++)
+			for (i = 0; i < NumberTabs; i++)
 			{
 				this->tabPage[i] = (gcnew System::Windows::Forms::TabPage());
 			};
@@ -128,7 +142,7 @@ namespace test1 {
 			// 
 			this->tabPage[0]->Paint += gcnew System::Windows::Forms::PaintEventHandler( this, &Form1::pagePaint );
 			
-			for(i = 0; i < n; i++)
+			for(i = 0; i < NumberTabs; i++)
 			{
 				this->tabControl1->Controls->Add(this->tabPage[i]);
 			};
@@ -139,22 +153,22 @@ namespace test1 {
 			this->tabControl1->Size = System::Drawing::Size(1700, 1350);
 			this->tabControl1->TabIndex = 0;
 			
-			for(i = 0; i < n; i++){
-			this->tabPage[i]->Location = System::Drawing::Point(4, 22);
-			this->tabPage[i]->Name = L"myTab";
-			this->tabPage[i]->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage[i]->Size = System::Drawing::Size(284, 238);
-			this->tabPage[i]->TabIndex = 0;
-			this->tabPage[i]->Text = "myTab "+ (char)(i+1);
-			this->tabPage[i]->UseVisualStyleBackColor = true;
-			this->tabPage[i]->Click += gcnew System::EventHandler(this, &Form1::tabPage1_Click);
-		//	this->tabPage[i]->Click += gcnew System::
+			for(i = 0; i < NumberTabs; i++)
+			{
+				this->tabPage[i]->Location = System::Drawing::Point(4, 22);
+				this->tabPage[i]->Name = L"myTab";
+				this->tabPage[i]->Padding = System::Windows::Forms::Padding(3);
+				this->tabPage[i]->Size = System::Drawing::Size(284, 238);
+				this->tabPage[i]->TabIndex = 0;
+				this->tabPage[i]->Text = "myTab "+ (char)(i+1);
+				this->tabPage[i]->UseVisualStyleBackColor = true;
+				this->tabPage[i]->Click += gcnew System::EventHandler(this, &Form1::tabPage1_Click);
 			};
 			
 			
 			// Form1
 			 
-			this->AutoScaleDimensions = System::Drawing::SizeF(m, t);
+			this->AutoScaleDimensions = System::Drawing::SizeF(SizeFormH, SizeFormW);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(292, 266);
 			this->Controls->Add(this->tabControl1);
@@ -166,10 +180,6 @@ namespace test1 {
 		}
 #pragma endregion
 	private: System::Void tabPage1_Click(System::Object^  sender, System::EventArgs^  e) {
-		//		Graphics^ g = f->Graphics;
-		//		g->DrawEllipse(System::Drawing::Brushes::Black, System::Drawing::Rectangle(5,5,4,4));
-				 //Form1 *form1 = new Form1(8);
-	
 			 }
 private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
 		 }
@@ -177,6 +187,9 @@ private: System::Void Form1_Load_1(System::Object^  sender, System::EventArgs^  
 		 }
 
 private: System::Void Form1_Load_2(System::Object^  sender, System::EventArgs^  e) {
+		 }
+
+private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e) {
 		 }
 };
 }

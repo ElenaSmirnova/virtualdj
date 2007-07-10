aac8c7bf124fad21d2322ee82c5273f4a87678b1
belkaitil <belkaitil@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-10 10:41:10 +0000

    the GUI was improved new connection with dlls

--- trunk/Sp/sound_processor/resource.h
+++ trunk/Sp/sound_processor/resource.h
@@ -5,8 +5,9 @@
 
 #define LENGTH 6 //16384
 #define CANALS 2
+[DllImport("wav.dll")]
+extern "C" void read2(char* name);
 
-//extern "C" __declspec(dllimport) void _cdecl read(char* name);
-extern "C" __declspec(dllimport) void _cdecl read2(char* name);
-extern "C" __declspec(dllimport) int mainEcho(int buffer[CANALS][LENGTH], float coefficient, bool flagOfFirstUse, int memoryBuffer[CANALS][LENGTH]);
-extern "C" __declspec(dllimport) int mainDistortion(int highLimit, int lowLimit, int buffer[CANALS][LENGTH]);
+[DllImport("sound_effects.dll")]
+extern "C" int mainEcho(int buffer[CANALS][LENGTH], float coefficient, bool flagOfFirstUse, int memoryBuffer[CANALS][LENGTH]);
+extern "C" int mainDistortion(int highLimit, int lowLimit, int buffer[CANALS][LENGTH]);

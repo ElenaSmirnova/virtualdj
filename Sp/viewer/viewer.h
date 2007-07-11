4118d21a2c69bf77c48f8a727fd30592a5f86a2f
AntalKina <AntalKina@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-11 10:59:00 +0000

    Look at all changed files from test1 and viewer projects.

--- Sp/viewer/viewer.h
+++ Sp/viewer/viewer.h
@@ -3,10 +3,11 @@
 
 #include <windows.h>
 #include <stdio.h>
+#include "..\wav\structure.h"
 
 //TODO:
 typedef  void view_parameters;
 
-extern "C" __declspec(dllexport) void view(view_parameters* parameters);
+extern "C" __declspec(dllexport) void view(Buffer* buf);
 
 

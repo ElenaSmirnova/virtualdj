9f16f41230d3937640c6190f51f2f6814e8d5fbe
pigulik <pigulik@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-13 09:14:01 +0000

    added function write2

--- Sp/wav_test/main.cpp
+++ Sp/wav_test/main.cpp
@@ -8,7 +8,9 @@ void main()
     //TODO:
     //вызов функции из динамической библиотеки wav
 	SoundBuffer *nullBuffer = new SoundBuffer(NULL, 0, 0);
-    read2(nullBuffer, "C:\\temp\\newVirtualDj\\sound\\asteroidexplode.wav");
+    read2(nullBuffer, "I:\\virtualdj\\Sp\\sound\\TC Delete Complete.wav");
+	write2(nullBuffer, NULL);
+	write2(nullBuffer, "I:\\virtualdj\\Sp\\sound\\TC Delete Complete.wav");
 
     printf("Stop\n");
 	getch();

74a19419b34d29df6a07618ab971794ed2185863
belkaitil <belkaitil@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-19 20:55:35 +0000

    new write2 and read2

--- Sp/wav/wav.cpp
+++ Sp/wav/wav.cpp
@@ -18,7 +18,7 @@ void appropriate(int **toArray, const int *fromArray,long int columns,int channe
 {
 	int i1 = 0;
 	if (channell == 2){
-		for (long int j = 0; j < columns; j ++){
+		for (long int j=0; j < 2*columns; j++){
 			toArray[i1][0] = fromArray[j];
 			j = j + 1;
 			toArray[i1][1] = fromArray[j];
@@ -30,36 +30,10 @@ void appropriate(int **toArray, const int *fromArray,long int columns,int channe
 			toArray[j][1] = NULL;
 		}
 	}
-}//разделяет одномерный массив на две строчки двумерного массива (8 bits)
-
-/*void appropriate2(int **toArray, short int *fromArray, long int columns,int channell)
-{
-	int i1 = 0;
-	if (channell == 2)
-	{
-		
-		for (long int j = 0; j < columns; j ++)
-		{
-			toArray[i1][0] = fromArray[j];
-			j = j + 1;
-			toArray[i1][1] = fromArray[j];
-			i1 = i1 + 1;
-		}
-	}
-	else 
-	{
-		for (long int j = 0; j < columns; j ++)
-		{
-			toArray[j][0] = fromArray[j];
-			toArray[j][1] = NULL;
-		}
-	} 
-}//разделяет одномерный массив на две строчки двумерного массива (16 bits)
-*/
+}//разделяет одномерный массив на две строчки двумерного массива
 
 extern "C" __declspec(dllexport) void read2(SoundBuffer *buffer, char* name)
 {
-	cout << "read\n";
 	FILE * f;
 	TitleWave tw;
 	
@@ -71,12 +45,11 @@ extern "C" __declspec(dllexport) void read2(SoundBuffer *buffer, char* name)
 	fread(&tw,sizeof(TitleWave),1,f);
 
 	long i=0;
-	int *sample = new int[1000000];
-	memset(sample, 0, 1000000*sizeof(int));
+	int *sample = new int[10000000];
+	memset(sample, 0, 10000000*sizeof(int));
 	while (!feof(f)){
 		if (tw.bits == 8){
 			fread(&sample[i], 1, 1, f);
-			cout << i << "   " << sample[i] << "\n";
 		}else{
 			if(tw.bits == 16){
 				fread(&sample[i], 2, 1, f);
@@ -87,8 +60,18 @@ extern "C" __declspec(dllexport) void read2(SoundBuffer *buffer, char* name)
 		i++;
 	}
 	
-	buffer->setLength(i-1);
-	appropriate(buffer->buff, sample, buffer->getLength(), tw.channels);
+	if (tw.channels == 1){
+		buffer->setLength(i-1);
+		appropriate(buffer->buff, sample, buffer->getLength(), tw.channels);
+	}else{
+		if (tw.channels == 2){
+			buffer->lastSimbol = i-1;
+			buffer->setLength((i-1)/2+1);
+			appropriate(buffer->buff, sample, buffer->getLength(), tw.channels);
+		}else{
+			return;
+		}
+	}
 
 	printf("LEN RIFF\t - %ld\n", tw.len_riff );
 	if ( strncmp(tw.id_riff,"RIFF",4)!=0 )
@@ -120,7 +103,6 @@ extern "C" __declspec(dllexport) void read2(SoundBuffer *buffer, char* name)
 //TODO:
 extern "C" __declspec(dllexport) void write2(SoundBuffer *buffer, char* name)
 {
-	cout << "write\n";
 	FILE *f;
 	
 	fopen_s(&f,name,"wb+"); //create new file if it doesn't exist, else overwrite the file
@@ -128,23 +110,27 @@ extern "C" __declspec(dllexport) void write2(SoundBuffer *buffer, char* name)
 
 	if (buffer->titleWave.bits == 8){
 		if (buffer->titleWave.channels == 2){
-			for (long int j = 0; j < buffer->getLength(); j++){
-				byte b = buffer->buff[j][0];
-				fwrite(&b, 1, 1, f);
+			for (long int j = 0; j < buffer->getLength()-1; j++){
+				fwrite(&buffer->buff[j][0], 1, 1, f);
 				fwrite(&buffer->buff[j][1], 1, 1, f);
+				if (2*j+3 == buffer->lastSimbol){
+						fwrite(&buffer->buff[j+1][0], 1, 1, f);
+				}
 			}
 		}else{
 			for (long int j = 0; j < buffer->getLength(); j ++){
-				byte b = buffer->buff[j][0];
-				fwrite(&b, 1, 1, f);
+				fwrite(&buffer->buff[j][0], 1, 1, f);
 			}
 		}
 	}else{
 		if (buffer->titleWave.bits == 16){
 			if (buffer->titleWave.channels == 2){
-				for (long int j = 0; j < buffer->getLength(); j ++){
+				for (long int j = 0; j < buffer->getLength()-1; j ++){
 					fwrite(&buffer->buff[j][0], 2, 1, f);
 					fwrite(&buffer->buff[j][1], 2, 1, f);
+					if (2*j+3 == buffer->lastSimbol){
+						fwrite(&buffer->buff[j+1][0], 2, 1, f);
+					}
 				}
 			}else{
 				for (long int j = 0; j < buffer->getLength(); j ++){
@@ -160,5 +146,3 @@ extern "C" __declspec(dllexport) void write2(SoundBuffer *buffer, char* name)
 #ifdef _MANAGED
 #pragma managed(pop)
 #endif
-
-

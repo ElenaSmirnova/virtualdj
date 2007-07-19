00ed9da5a8f4bd7f0d221e3e7f907b1db2b3e3a6
belkaitil <belkaitil@8565f2c5-1a2c-0410-8cfb-91c1cb6fbed1>
2007-07-19 10:10:42 +0000

    new read2 and write2

--- trunk/Sp/wav_test/main.cpp
+++ trunk/Sp/wav_test/main.cpp
@@ -7,10 +7,9 @@ void main()
     printf("Start\n");
     //TODO:
     //вызов функции из динамической библиотеки wav
-	SoundBuffer *nullBuffer = new SoundBuffer(NULL, 0, 0);
-    read2(nullBuffer, "I:\\virtualdj\\Sp\\sound\\TC Delete Complete.wav");
-	write2(nullBuffer, NULL);
-	write2(nullBuffer, "I:\\virtualdj\\Sp\\sound\\TC Delete Complete.wav");
+	SoundBuffer *nullBuffer = new SoundBuffer(NULL, 0);
+    read2(nullBuffer, "C:\\temp\\newVirtualDj\\sound\\ARROW.wav");
+	write2(nullBuffer, "C:\\temp\\newVirtualDj\\sound\\try.wav");
 
     printf("Stop\n");
 	getch();
